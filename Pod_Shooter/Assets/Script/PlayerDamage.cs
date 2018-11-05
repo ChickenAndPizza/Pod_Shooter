e7a45a3e715a4dd0c7910037714b7d000eb51b8c
B533P-05\533 <alessard97@hotmail.com>
2018-11-05 16:16:44 -0500

    Multiplayer

---
+++ Pod_Shooter/Assets/Script/PlayerDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour, Damagable {
+    PlayerLife playerLife;
+
+    public void DealDamage(int Damage)
+    {
+        playerLife.TakeDamage(Damage);
+    }
+
+    void Start () {
+        playerLife = GetComponent<PlayerLife>();
+	}
+	
+	void Update () {
+		
+	}
+}
